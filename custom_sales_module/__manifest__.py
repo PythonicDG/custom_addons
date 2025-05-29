75c33c974b388750c6318c88ddfe7355f3307609
dipak kashinath gaikwad <dipakgaikwadmg@gmail.com>
2025-05-29 12:24:02 +0530

    Created new custom sales module

---
+++ custom_sales_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Sales Module',
+    'version': '1.0',
+    'depends': ['sale'],
+    'data': [
+        'views/sales_person_form.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
