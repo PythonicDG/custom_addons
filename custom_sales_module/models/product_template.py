ecc034010dafa3d7025746df3d27462ba4d4dcc0
dipak kashinath gaikwad <dipakgaikwadmg@gmail.com>
2025-06-17 17:58:54 +0530

    store the values

--- custom_sales_module/models/product_template.py
+++ custom_sales_module/models/product_template.py
@@ -40,7 +40,43 @@ class ProductTemplate(models.Model):
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    custom_field_1 = fields.Boolean(string="Enable settings")
-    custom_field_2 = fields.Char(string="custom_field_2")
-    custom_field_3 = fields.Boolean(string="custom_field_3")
+    custom_field_1 = fields.Boolean(
+        string="Enable Setting",
+        config_parameter='custom_sales_module.custom_field_1'
+    )
+    
+    custom_field_2 = fields.Char(
+        string="Custom Field 2",
+        config_parameter='custom_sales_module.custom_field_2'
+    )
+    
+    custom_field_3 = fields.Boolean(
+        string="Custom Field 3",
+        config_parameter='custom_sales_module.custom_field_3'
+    )
+
+
+
+    # def set_values(self):
+    #     super().set_values()
+    #     self.env['ir.config_parameter'].sudo().set_param('custom_sales_module.custom_field_1', self.custom_field_1)
+    #     self.env['ir.config_parameter'].sudo().set_param('custom_sales_module.custom_field_2', self.custom_field_2)
+    #     self.env['ir.config_parameter'].sudo().set_param('custom_sales_module.custom_field_3', self.custom_field_3)
+
+    # @api.model
+    # def get_values(self):
+    #     res = super().get_values()
+    #     config = self.env['ir.config_parameter'].sudo()
+
+    #     res.update(
+    #         custom_field_1=config.get_param('custom_sales_module.custom_field_1') == 'True',
+    #         custom_field_2=config.get_param('custom_sales_module.custom_field_2', default=''),
+    #         custom_field_3=config.get_param('custom_sales_module.custom_field_3') == 'True',
+    #     )
+    #     return res
+
+
+
+
+
 
